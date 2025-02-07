3e52e2315f58e4fd397ea85d7db1d7b935b4d5b4
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2025-02-07 18:27:16 +0200

    save upd 🫡

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,65 +1,63 @@
 import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import sty from "./style.module.css"
-import del from "./deletebutton.svg"
+import sty from './style.module.css'
+import del from './deletebutton.svg'
+import { addNumber, delNum } from './redux/task/actions'
 const Counter = () => {
 	const phoneData = useSelector((state) => {
-		return state.contacts
+		return state.contacts.contacts
 	})
 
 	const dispatch = useDispatch()
 
-	function awdawd(e, nameBB, numBB) {
-		e.preventDefault()
 
-		const maxId = phoneData.reduce((max, contact) => {
-			return contact.id > max ? contact.id : max
-		}, 0)
-		dispatch({
-			type: 'addBB',
-			addContacts: {
-				name: nameBB,
-				number: numBB,
-				id: maxId + 1,
-			},
-		})
-	}
-	function delNum(e) {
-		dispatch({ type: 'minBB', id: e })
-	}
 	return (
 		<>
 			<h1>Number Phone</h1>
 			{console.log(phoneData)}
 
-			<form className={sty.form}
+			<form
+				className={sty.form}
 				action="addPhone"
 				onSubmit={(e) =>
-					awdawd(
+					dispatch(addNumber(
 						e,
 						document.getElementById('name').value,
 						document.getElementById('number').value
-					)
+					))
+					
 				}
 			>
-				<input type="text" className={sty.input} placeholder="number" id="number" />
-				<input type="text" className={sty.input} placeholder="name" id="name" />
+				<input
+					maxLength="15"
+					type="text"
+					className={sty.input}
+					placeholder="number"
+					id="number"
+				/>
+				<input
+					maxLength="10"
+					type="text"
+					className={sty.input}
+					placeholder="name"
+					id="name"
+				/>
 				<button className={sty.subAtn}>submit</button>
 			</form>
 
-			<ul  className={sty.list}>
+			<ul className={sty.list}>
 				{phoneData.map((data) => {
 					return (
 						<li className={sty.listItem} key={data.id}>
 							<p>{data.name}</p>
 							<p>{data.number}</p>
 							<button
-className={sty.deleteBtn}
+								className={sty.deleteBtn}
 								onClick={() => {
-									delNum(data.id)
+								dispatch(delNum(data.id))	
 								}}
 							>
-								<img  className={sty.deleteImg} src={del}  />
+								<img className={sty.deleteImg} src={del} />
 							</button>
 						</li>
 					)
--- src/store.js
+++ src/store.js
@@ -1,38 +1,44 @@
 import { createStore } from 'redux'
 import { composeWithDevTools } from '@redux-devtools/extension'
-const initialState = {
-	contacts: [
-		{
-			name: 'Stepan',
-			number: 80934248,
-			id: 1,
-		},
-		{
-			name: 'Tolik',
-			number: 345534354,
-			id: 2,
-		},
-	],
-	filter: 'awd',
-}
+import { configureStore } from '@reduxjs/toolkit'
+import { taskReduser } from './redux/task/reduser'
 
-const reduser = (state = initialState, action) => {
-	if (action.type === 'addBB') {
-		console.log(initialState)
-		
-		return {
-			contacts: [...state.contacts, action.addContacts],
-		}
-	} else if(action.type === "minBB") {
-		return {
-			...state,
-			contacts: state.contacts.filter(contact => contact.id !== action.id)
-		}
-	} else {
-		return state
-	}
-}
-console.log(initialState)
-const store = createStore(reduser, initialState, composeWithDevTools())
+const store = configureStore({
+	reducer: {
+		contacts: taskReduser,
+	},
+})
+export { store }
+// const initialState = {
 
-export default store
+// 	contacts: [
+// 		{
+// 			name: 'Stepan',
+// 			number: 80934248,
+// 			id: 1,
+// 		},
+// 		{
+// 			name: 'Tolik',
+// 			number: 345534354,
+// 			id: 2,
+// 		},
+// 	],
+// }
+
+// const reduser = (state = initialState, action) => {
+// 	if (action.type === 'addBB') {
+// 		console.log(initialState)
+
+// 		return {
+// 			contacts: [...state.contacts, action.addContacts],
+// 		}
+// 	} else if (action.type === 'minBB') {
+// 		return {
+// 			...state,
+// 			contacts: state.contacts.filter((contact) => contact.id !== action.id),
+// 		}
+// 	} else {
+// 		return state
+// 	}
+// }
+// console.log(initialState)
